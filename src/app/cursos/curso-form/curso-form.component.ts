36c6f5b074993d1899d0998a8327ba96484be5fe
Evandro C. Severginini <evandrosevergnini@gmail.com>
2020-10-18 15:06:23 -0300

    Atualizando curso

--- src/app/cursos/curso-form/curso-form.component.ts
+++ src/app/cursos/curso-form/curso-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
 import { Curso } from '../curso';
+import { CursoService } from '../services/curso.service';
 
 @Component({
   selector: 'curso-form',
@@ -12,15 +14,18 @@ export class CursoFormComponent implements OnInit {
   curso: Curso;
   categories = ['BEGINNER', 'INTERMEDIATE', 'ADVANCED'];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cursoService: CursoService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(
       (payload) => this.curso = payload.curso)
   }
 
-  salvaCurso() {
+  atualizaCurso() {
     console.log(this.curso);
+    this.cursoService.atualizaCurso(this.curso)
+    .pipe(first())
+    .subscribe();
   }
 
 }
--- src/app/cursos/services/curso.service.ts
+++ src/app/cursos/services/curso.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Curso } from '../curso';
@@ -7,14 +7,21 @@ import { Curso } from '../curso';
   providedIn: 'root',
 })
 export class CursoService {
-
   constructor(private http: HttpClient) {}
 
   carregaCursoPorId(id: string): Observable<Curso> {
-    return this.http.get<Curso>(`http://localhost:3000/cursos/${id}`)
+    return this.http.get<Curso>(`http://localhost:3000/cursos/${id}`);
   }
   carregaCursos(): Observable<Curso[]> {
     return this.http.get<Curso[]>('http://localhost:3000/cursos');
   }
 
+  atualizaCurso(curso: Curso): Observable<Curso> {
+    const headers = new HttpHeaders().set('Content-type', 'application/json');
+    return this.http.put<Curso>(
+      `http://localhost:3000/cursos/${curso.id}`,
+      curso,
+      { headers }
+    );
+  }
 }
