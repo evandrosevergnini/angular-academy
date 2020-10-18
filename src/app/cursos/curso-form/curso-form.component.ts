937caa8a77bff6212d435863f0818d7b8aad348e
Evandro C. Severginini <evandrosevergnini@gmail.com>
2020-10-18 12:14:45 -0300

    Adicionando formulário para Cursos

--- src/app/cursos/curso-form/curso-form.component.ts
+++ src/app/cursos/curso-form/curso-form.component.ts
@@ -10,12 +10,17 @@ import { Curso } from '../curso';
 export class CursoFormComponent implements OnInit {
 
   curso: Curso;
+  categories = ['BEGINNER', 'INTERMEDIATE', 'ADVANCED'];
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(
       (payload) => this.curso = payload.curso)
+  }
+
+  salvaCurso() {
+    console.log(this.curso);
   }
 
 }
--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -6,10 +6,11 @@ import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosComponent } from './cursos.component';
 import { CursoCardComponent } from './curso-card/curso-card.component';
 import { CursoFormComponent } from './curso-form/curso-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CursosComponent, CursoCardComponent, CursoFormComponent],
-  imports: [CommonModule, CursosRoutingModule, HttpClientModule],
+  imports: [CommonModule, CursosRoutingModule, HttpClientModule, FormsModule],
   providers: [HttpClient],
   exports: [CursosComponent]
 })
