2674f45e25a5b011c2590879b1e1641fce08c12b
Evandro C. Severginini <evandrosevergnini@gmail.com>
2020-10-18 23:19:49 -0300

    Refatorando

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-academy'`, () => {
+  it(`should have as title 'Angular Academy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-academy');
+    expect(app.title).toEqual('Angular Academy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-academy app is running!');
+    expect(compiled.querySelector('header h1 span').textContent).toContain('Angular Academy');
   });
 });
