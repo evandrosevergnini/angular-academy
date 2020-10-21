5ef772d285df05da1189ece717353685c25d3592
Evandro C. Severginini <evandrosevergnini@gmail.com>
2020-10-21 10:36:34 -0300

    OnChanges e ngModelChange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Filho } from './meu-primogenito/filho';
 
 @Component({
@@ -6,7 +6,26 @@ import { Filho } from './meu-primogenito/filho';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+
   title = 'Angular Academy';
   filho = new Filho('Evandro');
+
+  ngOnInit(): void {
+    const ctx = this;
+    let counter = 0;
+    const id = setInterval( () => {
+      counter += + 1;
+      if (Date.now() % 2 === 0){
+        ctx.filho = new Filho('Antonio');
+      } else {
+        ctx.filho = new Filho('Natanael');
+      }
+      if (counter >= 5) {
+        clearInterval(id)
+      }
+    },5000);
+
+  }
+
 }
--- src/app/meu-primogenito/meu-primogenito.component.ts
+++ src/app/meu-primogenito/meu-primogenito.component.ts
@@ -1,23 +1,44 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { Filho } from './filho';
 
 @Component({
   selector: 'meu-primogenito',
   templateUrl: './meu-primogenito.component.html',
-  styleUrls: ['./meu-primogenito.component.scss']
+  styleUrls: ['./meu-primogenito.component.scss'],
 })
-export class MeuPrimogenitoComponent implements OnInit {
+export class MeuPrimogenitoComponent implements OnInit, OnChanges {
 
   @Input()
   filho: Filho;
 
+  neto: Filho = new Filho('Platão');
+  @ViewChild('filhoEl') filhoEl;
   constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.dir(changes);
+    // for (const property in changes) {
+    //   console.log(property);
+    //   console.log(changes[property]);
+    // }
   }
 
   onInput(ev: KeyboardEvent) {
-    this.filho.nome = (ev.target as HTMLInputElement).value
+    this.filho.nome = (ev.target as HTMLInputElement).value;
+  }
+
+  onModelChange(ev: string) {
+    console.log(ev);
   }
 
 }
